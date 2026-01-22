f208fd3f4d7cb1582e9b2e06496a8ed939a7c365
Zidan <mzidanalghifari517@gmail.com>
2026-01-22 19:21:05 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+let passwordLenght = document.getElementById("passwordLenght");
+let password = document.getElementById("password");
+let saveButton = document.getElementById("saveButton");
+
+function generatorPassword(len) {
+    const lowAlphabet = "abcdefghijklmnopqrstuvwxyz";
+    const upperAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const numeric = "1234567890";
+    const symbol = "~!@#$%^&*()_-+=[{]};:'<>,.?/`";
+
+    const data = lowAlphabet + upperAlphabet + numeric + symbol;
+    let generator = '';
+    for (let index = 0; index < len; index++) {
+        generator += data[~~(Math.random() * data.length)];
+    }
+    return generator;
+}
+
+function getPassword() {
+    if (passwordLenght.value == "") {
+        alert("Masukkan Panjang password!");
+        return;
+    } else {
+        const newPassword = generatorPassword(passwordLenght.value);
+        password.value = newPassword;
+        alert('password berhasil dibuat!')
+    }
+}
+
+function savePassword() {
+    document.title = password.value;
+    saveButton.setAttribute(
+        'href',
+        'data:text/plain;charset=utf-8,' + encodeURIComponent(password.value)
+    );
+    saveButton.setAttribute('download', 'myPassword.txt');
+}
